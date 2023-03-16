88040d8aa0902158461a1c93771ff9358436240a
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 14:50:51 +0300

    set the landing coponent

--- src/Pages/Landing/landing.js
+++ src/Pages/Landing/landing.js
@@ -2,9 +2,13 @@ import Footer from "../../Components/Static/footer/footer";
 import Header from "../../Components/Static/header/header";
 
 export default function Landing() {
+  function Landing() {
+    return <main id="landing"> This is the Landing content</main>;
+  }
   return (
     <>
       <Header />
+      <Landing />
       <Footer />
     </>
   );
