d7ace5b9493cbbf6a2e150a663d72336d6fe4d4f
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 08:13:52 +0300

    create structure of the header

--- src/Components/Static/header/header.js
+++ src/Components/Static/header/header.js
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
-  return <header>This is the header</header>;
+  return (
+    <header id="header">
+      <nav>
+        <Link id="nav-logo" to="/">
+          Logo
+        </Link>
+
+        <span id="nav-cont">
+          <Link to="">Home</Link>
+          <Link to="">About Us</Link>
+          <Link to="">Contact Us</Link>
+        </span>
+
+        <button id="header-login">Login</button>
+      </nav>
+    </header>
+  );
 }
