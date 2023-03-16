ff30f5dd3c7afb95558c74b5656a7ac694816c6a
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 21:18:38 +0300

    create employee profile and add it to home

--- src/Pages/employee/home/home.js
+++ src/Pages/employee/home/home.js
@@ -1,10 +1,15 @@
 import "./home.css";
 import EmployeeNavBar from "../navbar/navbar";
+import EmployeeProfile from "../profile/profile";
+
 export default function EmployeeHome() {
   return (
     <section id="employee-cont">
       <EmployeeNavBar />
-      <section id="employee-home">This is the employee home</section>
+      <section id="employee-home">
+        <header id="employee-home-header"></header>
+      </section>
+      <EmployeeProfile />
     </section>
   );
 }
