4aff6b3f2067777236f7b7445afc1a22c364ce26
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 09:02:34 +0300

    create fucntion to handle login of user

--- src/Components/Static/header/header.js
+++ src/Components/Static/header/header.js
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 import "./header.css";
 
 export default function Header() {
+  let navigate = useNavigate();
+
+  function handleLogin() {
+    navigate("/login");
+  }
   return (
     <header id="header">
       <nav>
@@ -17,7 +24,9 @@ export default function Header() {
 
         <button id="menu-button">Menu</button>
 
-        <button id="header-login">Login</button>
+        <button id="header-login" onClick={handleLogin}>
+          Login
+        </button>
       </nav>
     </header>
   );
