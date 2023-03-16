6b8282417bb6281d1c827ddfbf20ddc7b28718f2
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 20:05:15 +0300

    create function to handle user login logic

--- src/Pages/general/auth/login.js
+++ src/Pages/general/auth/login.js
@@ -1,6 +1,15 @@
 import "./auth.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  let navigate = useNavigate();
+
+  function handleLogin(e) {
+    e.preventDefault();
+    navigate("/employee/home");
+  }
+
   return (
     <main id="login">
       <div></div>
@@ -8,7 +17,9 @@ export default function Login() {
         <h4>Login Into your Account</h4>
         <input type="text" placeholder="Email Address" />
         <input type="text" placeholder="Password" />
-        <button type="submit">Login</button>
+        <button type="submit" onClick={handleLogin}>
+          Login
+        </button>
         <Link to="reset">Forgot password?</Link>
       </form>
     </main>
