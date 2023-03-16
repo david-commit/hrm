64c35f60501afa7bd09e07c400fcb309de0ef92d
Enock <enock.mokua@student.moringaschool.com>
2023-03-16 15:35:40 +0300

    create login files

---
+++ src/Pages/auth/login.js
@@ -0,0 +1,17 @@
+import "./auth.css";
+
+export default function Login() {
+  return (
+    <main>
+      <div></div>
+      <form>
+        <p>Login Into your Account</p>
+        <span>
+          <label></label>
+          <input type="text" />
+        </span>
+        <button type="submit">Login</button>
+      </form>
+    </main>
+  );
+}
