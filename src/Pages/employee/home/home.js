4d48315791185f595e4adb529cd9afe5a4b4585e
Enock <enock.mokua@student.moringaschool.com>
2023-03-17 13:28:54 +0300

    create basic structure for home page

--- src/Pages/employee/home/home.js
+++ src/Pages/employee/home/home.js
@@ -14,6 +14,33 @@ export default function EmployeeHome() {
             <input type="text" placeholder="Search for anything" />
           </form>
         </header>
+
+        <div id="employee-home-stats">
+          <div id="employee-home-stats1">
+            <span className="employee-home-stats-bars"></span>
+            <span className="employee-home-stats-bars"></span>
+            <span className="employee-home-stats-bars"></span>
+          </div>
+          <div id="employee-home-stats2">
+            <p>Statistics for July</p>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div id="employee-home-schedule">
+          <div id="upcoming-tt">
+            <h4>Upcoming Activity</h4>
+
+          </div>
+          <div id="upcoming-schedule">
+            <h4>Upcoming Schedule</h4>
+            <span className="upcoming-schedule-cont"></span>
+            <span className="upcoming-schedule-cont"></span>
+            <span className="upcoming-schedule-cont"></span>
+            <button id="upcoming-schedule-link">See all Activity</button>
+          </div>
+        </div>
       </section>
       <EmployeeProfile />
     </section>
