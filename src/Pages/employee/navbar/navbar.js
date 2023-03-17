a760a4699d48bd264dca860e764a02011ba7fa55
Enock <enock.mokua@student.moringaschool.com>
2023-03-17 13:33:41 +0300

    create basic links for employee navbar

--- src/Pages/employee/navbar/navbar.js
+++ src/Pages/employee/navbar/navbar.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export default function EmployeeNavBar() {
-  return <nav id="employee-nav">this is the nvabar</nav>;
+  return (
+    <nav id="employee-nav">
+      <p>HRTech Int.</p>
+      <Link to="/employee/home">Home</Link>
+      <Link to="/employee/profile">Profile</Link>
+      <Link to="/employee/money">Money</Link>
+      <Link to="/employee/schedule">Schedule</Link>
+    </nav>
+  );
 }
