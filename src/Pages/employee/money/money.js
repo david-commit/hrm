22b81ae1e802d6ec36b3ee8a8da867100f6cf6f2
Enock <enock.mokua@student.moringaschool.com>
2023-03-17 21:18:44 +0300

    create charts folders

--- src/Pages/employee/money/money.js
+++ src/Pages/employee/money/money.js
@@ -2,8 +2,45 @@ import EmployeeNavBar from "../navbar/navbar";
 import "./oney.css";
 import React from "react";
 import { FaDownload } from "react-icons/fa";
+import { Bar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function EmployeeMoney() {
+  const data = {
+    labels: ["Item 1", "Item 2"],
+    datasets: [
+      {
+        label: "Item 1",
+        data: [82, 100],
+        borderColor: "black",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Item 2",
+        data: [40, 80],
+        borderColor: "black",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
   return (
     <section id="employee-cont">
       <EmployeeNavBar />
@@ -29,7 +66,12 @@ export default function EmployeeMoney() {
         <div id="employee-money-cont2">
           <div id="employee-money-cont2-1">
             <p>Last weeks overview</p>
-            <div id="employee-last-chart"></div>
+            <Bar
+              data={data}
+              options={{
+                responsive: true,
+              }}
+            />
           </div>
 
           <div id="employee-money-cont2-2">
