bf3aa76af0ea7e5162e35322ba2459517a540c11
Enock <enock.mokua@student.moringaschool.com>
2023-03-17 21:28:15 +0300

    create client hoem and client scetion components

---
+++ src/Pages/client/home/home.js
@@ -0,0 +1,5 @@
+import "./home.css";
+
+export default function ClientHome() {
+  return <section id="client-cont"></section>;
+}
---
+++ src/Pages/client/money/money.js
@@ -0,0 +1,5 @@
+import "./money.css";
+
+export default function ClientMoney() {
+  return <section id="client-cont"></section>;
+}
