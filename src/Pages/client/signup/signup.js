63850ecafe7defdacd3f52e968fe61a8e6200c14
Enock <enock.mokua@student.moringaschool.com>
2023-03-18 23:16:56 +0300

    create header structure of signup client page

--- src/Pages/client/signup/signup.js
+++ src/Pages/client/signup/signup.js
@@ -4,7 +4,29 @@ export default function ClientSignup() {
   return (
     <section id="client-cont">
       <ClientBar />
-      <section id="client-signup">client signup page</section>
+      <section id="client-signup">
+        <header id="client-signup-header">
+          <span className="client-signup-header-span">
+            Hello there please fill in your organizations details.
+          </span>
+        </header>
+
+        <div id="client-signup-download">
+          <p>Download this form and fill out your employees data</p>
+          <button id="download-excel" type="download">
+            Download data
+          </button>
+        </div>
+
+        <form id="client-signup-form">
+          <div class="upload-btn-wrapper">
+            <button class="btn">Upload a file</button>
+            <input type="file" name="myfile" />
+          </div>
+        </form>
+
+        
+      </section>
     </section>
   );
 }
