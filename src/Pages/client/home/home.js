098a44bb9a24ce248c23694827e241bde0308620
Enock <enock.mokua@student.moringaschool.com>
2023-03-18 09:35:59 +0300

    create structure for client home header

--- src/Pages/client/home/home.js
+++ src/Pages/client/home/home.js
@@ -1,11 +1,31 @@
 import ClientBar from "../navbar/navbar";
 import "./home.css";
 
+import { FaBell } from "react-icons/fa";
+
 export default function ClientHome() {
   return (
     <section id="client-cont">
       <ClientBar />
-      <section id="client-home">client home</section>
+      <section id="client-home">
+        <header>
+          <span>
+            <h3>Good morning, Hr. Manager</h3>
+            <p>Hope you have a wonderful day</p>
+          </span>
+
+          <span>
+            <form>
+              <button>Search</button>
+              <input type="text" placeholder="Search for anything ..." />
+            </form>
+
+            <button>
+              <FaBell />
+            </button>
+          </span>
+        </header>
+      </section>
     </section>
   );
 }
