13762facbf2feb60f8a8ac61e85b785a68807a12
Enock <enock.mokua@student.moringaschool.com>
2023-03-19 00:00:02 +0300

    create structre for form upload details

--- src/Pages/client/signup/signup.js
+++ src/Pages/client/signup/signup.js
@@ -19,17 +19,57 @@ export default function ClientSignup() {
         </div>
 
         <form id="client-signup-form">
-          <p>Upload the data sheet here</p>
-          <input
-            className="custom-file-upload"
-            type="file"
-            accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-          />
+          <div id="client-signup-form-upload">
+            <p>Upload the data sheet here</p>
+            <input
+              className="custom-file-upload"
+              type="file"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            />
+          </div>
+
+          <div id="client-signup-form-details">
+            <span>
+              <label>Organisation name</label>
+              <input type="text" placeholder="Organisation name." />
+            </span>
+
+            <span>
+              <label> Brief description of organisation</label>
+              <input
+                type="text"
+                placeholder="Brief descritpion of your organisation"
+              />
+            </span>
+
+            <span>
+              <label for="org-type">Select organization type:</label>
+              <select id="org-type" name="org-type">
+                <option value="sole-proprietorship">Sole Proprietorship</option>
+                <option value="partnership">Partnership</option>
+                <option value="corporation">Corporation</option>
+                <option value="limited-liability-company">
+                  Limited Liability Company (LLC)
+                </option>
+                <option value="nonprofit-organization">
+                  Nonprofit Organization
+                </option>
+                <option value="cooperative">Cooperative</option>
+                <option value="franchise">Franchise</option>
+                <option value="joint-venture">Joint Venture</option>
+                <option value="government-agency">Government Agency</option>
+                <option value="international-organization">
+                  International Organization
+                </option>
+              </select>
+            </span>
+            
+          </div>
 
           <button type="submit">Submit</button>
         </form>
 
-        <></>
+        <form type=""></form>
       </section>
     </section>
   );
