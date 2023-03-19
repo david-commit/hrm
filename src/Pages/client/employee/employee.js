66132aa6b726a5fad04f64b13d10c437c9fe70b6
Enock <enock.mokua@student.moringaschool.com>
2023-03-19 12:17:40 +0300

    create the basic structire of the client employeee section

--- src/Pages/client/employee/employee.js
+++ src/Pages/client/employee/employee.js
@@ -1,3 +1,4 @@
+import ClientHeader from "../home/header";
 import ClientBar from "../navbar/navbar";
 import "./employee.css";
 
@@ -5,7 +6,19 @@ export default function ClientEmployee() {
   return (
     <section id="client-cont">
       <ClientBar />
-      <section id="client-employee">client employee</section>
+
+      <section id="client-employee">
+        <ClientHeader />
+        <div id="client-employee-stats">
+          <div id="client-employee-attendace"></div>
+          <div id="client-employee-payroll"></div>
+        </div>
+
+        <div id="client-employee-info">
+          <div id="client-employee-cases"></div>
+          <div id="client-employee-location"></div>
+        </div>
+      </section>
     </section>
   );
 }
