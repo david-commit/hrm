2400156cc56e8e098622361b24e85a9448396ed8
Enock <enock.mokua@student.moringaschool.com>
2023-03-19 13:06:59 +0300

    create container structure

--- src/Pages/client/employee/employee.js
+++ src/Pages/client/employee/employee.js
@@ -1,6 +1,8 @@
 import ClientHeader from "../home/header";
 import ClientBar from "../navbar/navbar";
 import "./employee.css";
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
 
 export default function ClientEmployee() {
   return (
@@ -10,7 +12,63 @@ export default function ClientEmployee() {
       <section id="client-employee">
         <ClientHeader message="Welcome to Enock's profile." />
         <div id="client-employee-stats">
-          <div id="client-employee-attendace">ddggdg</div>
+          <div id="client-employee-attendace">
+            <h3>March in Stats</h3>
+            <div id="client-employee-attendace-cont">
+              <span className="attendace-cont">
+                <p>Days present</p>
+                <span>
+                  <p>
+                    25 <i>days</i>
+                  </p>
+
+                  <span className="att-up">
+                    <FaArrowUp />
+                    <p>10%</p>
+                  </span>
+                </span>
+              </span>
+              <span className="attendace-cont">
+                <p>Days present</p>
+                <span>
+                  <p>
+                    25 <i>days</i>
+                  </p>
+
+                  <span className="att-up">
+                    <FaArrowUp />
+                    <p>10%</p>
+                  </span>
+                </span>
+              </span>
+              <span className="attendace-cont">
+                <p>Days present</p>
+                <span>
+                  <p>
+                    25 <i>days</i>
+                  </p>
+
+                  <span className="att-up">
+                    <FaArrowUp />
+                    <p>10%</p>
+                  </span>
+                </span>
+              </span>
+              <span className="attendace-cont">
+                <p>Days present</p>
+                <span>
+                  <p>
+                    25 <i>days</i>
+                  </p>
+
+                  <span className="att-up">
+                    <FaArrowUp />
+                    <p>10%</p>
+                  </span>
+                </span>
+              </span>
+            </div>
+          </div>
           <div id="client-employee-payroll"></div>
         </div>
 
