3ec1b7de24b5cf1477ff5dfc3766d565d7d06106
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 00:33:21 +0300

    create generate pdf functon for payslip

--- src/Components/finance/payslip.js
+++ src/Components/finance/payslip.js
@@ -1,5 +1,34 @@
 import "./fin.css";
 
-export default function Payslip() {
+import React from "react";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
+
+export default function Payslip({ employee }) {
+  let generatePayslip = () => {
+    let doc = new jsPDF();
+    doc.text(`Payslip for ${employee.name}`, 10, 10);
+    doc.text("Earnings", 10, 20);
+    const earningsData = employee.earnings.map((e) => [
+      e.description,
+      e.amount,
+    ]);
+    doc.autoTable({
+      startY: 25,
+      head: [["Description", "Amount"]],
+      body: earningsData,
+    });
+    doc.text("Deductions", 10, doc.autoTable.previous.finalY + 10);
+    const deductionsData = employee.deductions.map((d) => [
+      d.description,
+      d.amount,
+    ]);
+    doc.autoTable({
+      startY: doc.autoTable.previous.finalY + 20,
+      head: [["Description", "Amount"]],
+      body: deductionsData,
+    });
+    doc.save("payslip.pdf");
+  };
   return <button></button>;
 }
