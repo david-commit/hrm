f4a464bb84494119445fa19893c266e506b3ad27
Enock <enock.mokua@student.moringaschool.com>
2023-03-19 17:02:44 +0300

    create structure for messages and add layout structure

--- src/Pages/client/notifications/notifs.js
+++ src/Pages/client/notifications/notifs.js
@@ -1,11 +1,14 @@
 import ClientHeader from "../home/header";
+import ClientBar from "../navbar/navbar";
 import "./notifs.css";
 
 export default function Notifications() {
   return (
     <section id="client-cont">
-      <ClientHeader />
-      <section id="client-notifs">notifications</section>
+      <ClientBar />
+      <section id="client-notifs" className="client-nots">
+        <ClientHeader message="Here are your notifications." />
+      </section>
     </section>
   );
 }
