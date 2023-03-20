8e2a0f61b69ef36b7521d4bbc87565fb8a55414c
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 15:15:12 +0300

    restructure client signup form

--- src/Pages/client/signup/signup.js
+++ src/Pages/client/signup/signup.js
@@ -1,10 +1,41 @@
 import "./signup.css";
 import ClientBar from "../navbar/navbar";
+import ClientHeader from "../home/header";
 export default function ClientSignup() {
   return (
     <section id="client-cont">
       <ClientBar />
-      <section id="client-signup"></section>
+      <section id="client-signup">
+        <ClientHeader message="Its payday approve payments" />
+        <form>
+          <h3>Set up dates for payment</h3>
+          <span></span>
+          <button type="submit">Update</button>
+        </form>
+
+        <div>
+          <h3>Approve payment</h3>
+          <span>
+            <p>Approve payment for casual employees</p>
+            <button>Approve</button>
+          </span>
+
+          <span>
+            <p>Approve payment for permanent employees</p>
+            <button>Approve</button>
+          </span>
+
+          <span>
+            <p>Approve payment for seasonal employees</p>
+            <button>Approve</button>
+          </span>
+
+          <span>
+            <p>Approve payment for casual</p>
+            <button>Approve</button>
+          </span>
+        </div>
+      </section>
     </section>
   );
 }
