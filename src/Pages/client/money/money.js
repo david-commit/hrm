a017140dfb80283209c03b1f6de0f0e07f9d6d6b
Enock <enock.mokua@student.moringaschool.com>
2023-03-20 13:58:59 +0300

    create structure for add employee

--- src/Pages/client/money/money.js
+++ src/Pages/client/money/money.js
@@ -1,3 +1,4 @@
+import ClientHeader from "../home/header";
 import ClientBar from "../navbar/navbar";
 import "./money.css";
 
@@ -5,7 +6,26 @@ export default function ClientMoney() {
   return (
     <section id="client-cont">
       <ClientBar />
-      <section id="client-money"></section>
+      <section id="client-money">
+        <ClientHeader />
+        <h3>Add new employee to your organisation</h3>
+        <form>
+          <h4>Mass add employees for your organisation.</h4>
+          <input type="file" />
+          <button type="submit">Add employees</button>
+        </form>
+        <form>
+          <h4>Add a single employee</h4>
+          <span>
+            <input type="text" placeholder="First name" />
+            <input type="text" placeholder="Second name" />
+            <input type="text" placeholder="Third name" />
+            <input type="number" placeholder="Id number" />
+          </span>
+
+          <button type="submit">Register employee</button>
+        </form>
+      </section>
     </section>
   );
 }
