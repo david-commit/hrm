8ced9f4e333b1f2d70ac30cb4dc4dab17849e604
david-commit <davidondiege@gmail.com>
2023-03-24 22:56:05 +0300

    Created employee activity tabs

--- src/Pages/employee/schedule/EmployeeSchedule.js
+++ src/Pages/employee/schedule/EmployeeSchedule.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './EmployeeSchedule.css';
 import EmployeeNavBar from '../navbar/navbar';
 import EmployeeHeader from '../header/EmployeeHeader';
+import {
+  Tabs,
+  TabList,
+  Tab,
+  TabPanel,
+} from 'https://cdn.skypack.dev/react-tabs@3.2.2';
 
 const EmployeeSchedule = () => {
   return (
@@ -10,7 +16,19 @@ const EmployeeSchedule = () => {
       <section className='employee-schedule-body'>
         <EmployeeHeader />
         <section className='employee-schedule'>
-        <h1>Your Activity</h1>
+          <h1>Your Activity</h1>
+          <Tabs>
+            <TabList className='mt--10'>
+              <Tab>Today's Tasks</Tab>
+              <Tab>Add New Task</Tab>
+              <Tab>Pending Requests</Tab>
+              <Tab>Apply for Leave</Tab>
+            </TabList>
+            <TabPanel>Content of tab 1</TabPanel>
+          
+            <TabPanel>Content of tab 3</TabPanel>
+            <TabPanel>Content of tab 3</TabPanel>
+          </Tabs>
         </section>
       </section>
     </div>
