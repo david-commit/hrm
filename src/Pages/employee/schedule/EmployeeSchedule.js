8a0119f18c823986804cac6e3439415b21c172ae
david-commit <davidondiege@gmail.com>
2023-03-24 23:17:35 +0300

    Complete Add New Task form creation

--- src/Pages/employee/schedule/EmployeeSchedule.js
+++ src/Pages/employee/schedule/EmployeeSchedule.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EmployeeSchedule.css';
 import EmployeeNavBar from '../navbar/navbar';
 import EmployeeHeader from '../header/EmployeeHeader';
@@ -10,6 +10,13 @@ import {
 } from 'https://cdn.skypack.dev/react-tabs@3.2.2';
 
 const EmployeeSchedule = () => {
+  const [newTask, setNewTask] = useState('')
+  const [taskDescription, setTaskDescription] = useState('')
+
+  const handleAddNewTask = e => {
+    e.preventDefault()
+  }
+
   return (
     <div className='employee-schedule-container'>
       <EmployeeNavBar />
@@ -25,7 +32,27 @@ const EmployeeSchedule = () => {
               <Tab>Apply for Leave</Tab>
             </TabList>
             <TabPanel>Content of tab 1</TabPanel>
-          
+            <TabPanel className='add-new-task-tab'>
+              <h4>Create New Task</h4>
+              <form onSubmit={handleAddNewTask}>
+                <label htmlFor='task'>Title</label>
+                <input
+                  type='text'
+                  name='task'
+                  placeholder='Enter title of the task'
+                  value={newTask}
+                  onChange={(e) => setNewTask(e.target.value)}
+                />
+                <label htmlFor='description'>Task Description (Optional)</label>
+                <textarea
+                  name='desctiption'
+                  placeholder='Write a description of the task'
+                  value={taskDescription}
+                  onChange={(e) => setTaskDescription(e.target.value)}
+                ></textarea>
+                <button type='submit'>Add Task</button>
+              </form>
+            </TabPanel>
             <TabPanel>Content of tab 3</TabPanel>
             <TabPanel>Content of tab 3</TabPanel>
           </Tabs>
