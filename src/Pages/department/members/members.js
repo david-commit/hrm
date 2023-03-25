c9d138389e92c77181af3fb8f8a0af596419da2a
Enock <enock.mokua@student.moringaschool.com>
2023-03-25 16:12:58 +0300

    create form fro serachin members

--- src/Pages/department/members/members.js
+++ src/Pages/department/members/members.js
@@ -10,6 +10,15 @@ export default function DepartmentMembers() {
         <DepartmentHeader
           message={"You can see all the information about your members"}
         />
+
+        <form id="member-search">
+          <h3>Search for members in your department</h3>
+          <p>Use either their name or employee number</p>
+          <span>
+            <button type="submit">Search</button>
+            <input type="search" />
+          </span>
+        </form>
       </section>
     </section>
   );
