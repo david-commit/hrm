fdbe348d11d1a3b36b038cf5ed9c89e0c5741a8f
Enock <enock.mokua@student.moringaschool.com>
2023-03-25 22:51:23 +0300

    create form section

--- src/Pages/department/members/detail.js
+++ src/Pages/department/members/detail.js
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import "./details.css";
+import DepartmentHeader from "../header/header";
+import DepartmentNav from "../navbar/navbar";
 
 const users = [
   { id: 1, name: "John Doe", email: "john.doe@example.com" },
@@ -10,18 +12,52 @@ const users = [
 export function MemberDetail() {
   let { userId } = useParams();
 
-  // Find the user by ID
   const user = users.find((u) => u.id === Number(userId));
-
   if (!user) {
     return <div>User not found</div>;
   }
 
   return (
-    <section>
-      <h1>User Detail</h1>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
+    <section className="department-cont">
+      <DepartmentNav />
+      <section id="department-member" className="member-cont">
+        <DepartmentHeader message={`Welcome to ${user.name}'s profile.`} />
+
+        <section id="member-home">
+          <span className="member-home-cont"></span>
+          <span className="member-home-cont"></span>
+          <span className="member-home-cont"></span>
+          <span className="member-home-cont"></span>
+        </section>
+
+        <section id="member-body">
+          <div id="member-body-head"></div>
+          <div id="member-body-body">
+            
+          </div>
+          <form id="member-body-objectives">
+            <h4>Assign new task.</h4>
+
+            <span>
+              <label>Enter task name</label>
+              <input type="text" placeholder="Task name" />
+            </span>
+
+            <span>
+              <label>Enter task description</label>
+              <input text="text" placeholder="Task description" />
+            </span>
+
+            <span>
+              <label>Enter deadline</label>
+              <input type="calendar" placeholder="Datetime" />
+            </span>
+
+            <button type="submit">Assign task</button>
+          </form>
+        </section>
+        <section id="member-foot"></section>
+      </section>
     </section>
   );
 }
