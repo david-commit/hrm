e4254b0e9d6bb43a9e945c5d852ba38068a513d9
Enock <enock.mokua@student.moringaschool.com>
2023-03-25 16:17:17 +0300

    create table compoent

---
+++ src/Pages/department/members/table.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Table({ data, columns }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th key={column}>{column}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row) => (
+          <tr key={row.id}>
+            {columns.map((column) => (
+              <td key={`${row.id}-${column}`}>{row[column]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
