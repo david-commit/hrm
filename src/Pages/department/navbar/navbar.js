d49dceb6aabac672f3e4fbd4c270d4c095ee0755
Enock <enock.mokua@student.moringaschool.com>
2023-03-25 15:47:46 +0300

    create links to various sections in the navbatr

--- src/Pages/department/navbar/navbar.js
+++ src/Pages/department/navbar/navbar.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
 export default function DepartmentNav() {
-  return <aside>This is the navbar</aside>;
+  return (
+    <aside id="department-nav">
+      <span id="department-nav-head">
+        <Link to="/">HRTech Int.</Link>
+      </span>
+      <span id="department-nav-body">
+        <Link to="">Apps</Link>
+        <Link to="">Home</Link>
+        <Link to="">Members</Link>
+        <Link to="">Profile</Link>
+      </span>
+      <span id="department-nav-foot">Logout</span>
+    </aside>
+  );
 }
