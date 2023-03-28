3a78b2dbafc821d60e7da4bf682ba62f534bf990
david-commit <davidondiege@gmail.com>
2023-03-28 23:44:38 +0300

    Styled the Client Approval page

--- src/Pages/client/ClientApprovals/ClientApprovals.js
+++ src/Pages/client/ClientApprovals/ClientApprovals.js
@@ -1,46 +1,48 @@
-import React from 'react'
+import React from 'react';
 import './ClientApprovals.css';
-import "../signup/signup.css";
-import ClientBar from "../navbar/navbar";
-import ClientHeader from "../home/header";
+import '../signup/signup.css';
+import ClientBar from '../navbar/navbar';
+import ClientHeader from '../home/header';
 
 const ClientApprovals = () => {
   return (
-    <section id="client-cont">
+    <section className='client-approvals-container'>
       <ClientBar />
-      <section id="client-signup">
-        <ClientHeader message="Its payday approve payments" />
-        <form>
-          <h3>Set up dates for payment</h3>
-          <span></span>
-          <button type="submit">Update</button>
-        </form>
+      <section className='client-approvals-body'>
+        <ClientHeader message='Its payday approve payments' />
+        <section className='client-approvals'>
+          <form>
+            <h3>Set up dates for payment</h3>
+            <input type='date' /> <br />
+            <button type='submit'>Update</button>
+          </form>
 
-        <div>
-          <h3>Approve payment</h3>
-          <span>
-            <p>Approve payment for casual employees</p>
-            <button>Approve</button>
-          </span>
+          <div>
+            <h3>Approve payment</h3>
+            <article>
+              <p>Approve payment for casual employees</p>
+              <button>Approve</button>
+            </article>
 
-          <span>
-            <p>Approve payment for permanent employees</p>
-            <button>Approve</button>
-          </span>
+            <article>
+              <p>Approve payment for permanent employees</p>
+              <button>Approve</button>
+            </article>
 
-          <span>
-            <p>Approve payment for seasonal employees</p>
-            <button>Approve</button>
-          </span>
+            <article>
+              <p>Approve payment for seasonal employees</p>
+              <button>Approve</button>
+            </article>
 
-          <span>
-            <p>Approve payment for casual employees</p>
-            <button>Approve</button>
-          </span>
-        </div>
+            <article>
+              <p>Approve payment for casual employees</p>
+              <button>Approve</button>
+            </article>
+          </div>
+        </section>
       </section>
     </section>
-  )
-}
+  );
+};
 
 export default ClientApprovals;
