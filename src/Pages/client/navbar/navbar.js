2dab192bf70c91014653b3be906ef047ad9efd9a
david-commit <davidondiege@gmail.com>
2023-03-28 22:46:22 +0300

    Added tooltip to client navbar

--- src/Pages/client/navbar/navbar.js
+++ src/Pages/client/navbar/navbar.js
@@ -1,36 +1,51 @@
-import { Link } from "react-router-dom";
-import "./navbar.css";
-import { RiLogoutBoxRLine } from "react-icons/ri";
-import { BiMoney } from "react-icons/bi";
-import { BsPeople } from "react-icons/bs";
-import { FaUserPlus } from "react-icons/fa";
-import { AiOutlineHome } from "react-icons/ai";
-import { FaUserCircle } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import './navbar.css';
+import { RiLogoutBoxRLine } from 'react-icons/ri';
+import { BiMoney } from 'react-icons/bi';
+import { BsPeople } from 'react-icons/bs';
+import { FaUserPlus } from 'react-icons/fa';
+import { AiOutlineHome } from 'react-icons/ai';
+import { FaUserCircle } from 'react-icons/fa';
+import { FiUserCheck } from 'react-icons/fi';
 
 export default function ClientBar() {
   return (
-    <aside id="client-nav-bar">
-      <span id="client-bar-head">HrTech Int.</span>
-      <span id="client-bar-info">
-        <Link to="/client/home">
+    <aside id='client-nav-bar'>
+      <span id='client-bar-head'>HrTech Int.</span>
+      <span id='client-bar-info'>
+        <Link to='/client/home' class='tooltip' id='employee-menu'>
+          {' '}
           <AiOutlineHome />
+          <span class='tooltiptext'>Home</span>
         </Link>
-        <Link to="/client/profile">
+        <Link to='/client/profile' class='tooltip' id='employee-menu'>
+          {' '}
           <FaUserCircle />
+          <span class='tooltiptext'>Profile</span>
         </Link>
-        <Link to="/client/money">
+        <Link to='/client/money' class='tooltip' id='employee-menu'>
+          {' '}
           <BiMoney />
+          <span class='tooltiptext'>Money</span>
         </Link>
-        <Link to="/client/employee">
+        <Link to='/client/employee' class='tooltip' id='employee-menu'>
+          {' '}
           <BsPeople />
+          <span class='tooltiptext'>Employee</span>
         </Link>
-        <Link to="/client/signup">
+        <Link to='/client/approvals' class='tooltip' id='employee-menu'>
+          {' '}
+          <FiUserCheck />
+          <span class='tooltiptext'>Approlals</span>
+        </Link>
+
+        {/* <Link to='/client/signup'>
           <FaUserPlus />
-        </Link>
+        </Link> */}
       </span>
 
-      <span id="client-bar-foot">
-        <Link to="/">
+      <span id='client-bar-foot'>
+        <Link to='/'>
           <RiLogoutBoxRLine />
         </Link>
       </span>
