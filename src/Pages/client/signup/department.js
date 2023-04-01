a09e1d41c1fdea1bbc4eeb6b742d6893ba6a6553
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 20:23:26 +0300

    create the structure for departent in client

--- src/Pages/client/signup/department.js
+++ src/Pages/client/signup/department.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import ClientBar from "../navbar/navbar";
 import ClientHeader from "../home/header";
+import "./dept.css";
 
 export default function DepartmentPay() {
   let { id } = useParams();
 
+  console.log(id);
   return (
     <section id="client-cont">
       <ClientBar />
@@ -13,9 +15,31 @@ export default function DepartmentPay() {
         <ClientHeader message="Its payday approve payments" />
         <section id="client-payment-cont">
           <div id="client-payment-cont1">
-            <h3>Release Employees Salaries</h3>
+            <h4>Engineering department</h4>
+            <div id="client-payment-cont-1-1">
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
+              <span className="client-payment-cont-1-1"></span>
+            </div>
+
+            <h5>Top Earners</h5>
+
+            <div id="client-payment-cont-1-2-head">
+              <span>Name</span>
+              <span>Days Present</span>
+              <span>Rating</span>
+              <span>Gross Pay</span>
+            </div>
+            <ol id="client-payment-cont-1-2">
+              <li className="client-payment-cont-1-2"></li>
+            </ol>
           </div>
-          <aside id="client-payment-cont2"></aside>
+
+          <aside id="client-payment-cont2">
+            <h4>Summary for this week</h4>
+            <button id="client-payment-button">Release Salary</button>
+          </aside>
         </section>
       </section>
     </section>
