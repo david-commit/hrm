165f8b3df49e25defd9f8b949b91256739799476
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 22:35:57 +0300

    post data  for attendace to backend

--- src/Pages/department/members/detail.js
+++ src/Pages/department/members/detail.js
@@ -34,6 +34,28 @@ export function MemberDetail() {
     let nairobiDate = new Date().toLocaleDateString("en-Us", {
       timeZone: "Africa/Nairobi",
     });
+
+    let data = {
+      employee_id: 1,
+      date: nairobiDate,
+      clock_in: attendanceData.clock_in,
+      time_out: attendanceData.clock_out,
+    };
+
+    fetch("http://localhost:3000/attendances", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   }
 
   // handle user not found
