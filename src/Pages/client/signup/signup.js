15eeb69d3838b70e06f54dfb46b8242abdcd0b12
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 03:52:07 +0300

    restructure the cdepartment container

--- src/Pages/client/signup/signup.js
+++ src/Pages/client/signup/signup.js
@@ -1,15 +1,38 @@
 import "./signup.css";
 import ClientBar from "../navbar/navbar";
 import ClientHeader from "../home/header";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+let data = [
+  {
+    department_id: "1001",
+    department_name: "Sales",
+  },
+  {
+    department_id: "1002",
+    department_name: "Marketing",
+  },
+  {
+    department_id: "1003",
+    department_name: "Finance",
+  },
+  {
+    department_id: "1004",
+    department_name: "Human Resources",
+  },
+  {
+    department_id: "1005",
+    department_name: "Information Technology",
+  },
+];
 
 export default function ClientSignup() {
   let navigate = useNavigate();
 
-  function redrectDepartment() {
+  function redrectToDepartment() {
     navigate("/client/signup/:id");
   }
-  
+
   return (
     <section id="client-cont">
       <ClientBar />
@@ -18,12 +41,21 @@ export default function ClientSignup() {
         <section id="client-payment-cont">
           <div id="client-payment-cont1">
             <h3>Release Employees Salaries</h3>
-            <div className="payment-list">
-              <span className="payment-list-head">1</span>
-              <span className="payment-list-body">
-                <p>Engineering Department</p>
-              </span>
-            </div>
+            {data.map((department) => (
+              <Link
+                className="payment-list"
+                key={department.department_id}
+                to={`client/signup/${department.department_id}`}
+              >
+                <span className="payment-list-head">
+                  {department.department_id}
+                </span>
+                <span className="payment-list-body">
+                  <p>{department.department_name}</p>
+                  {/* <button onClick={() => redirectToDepartment(department.department_id)}>Sign up</button> */}
+                </span>
+              </Link>
+            ))}
           </div>
           <aside id="client-payment-cont2"></aside>
         </section>
