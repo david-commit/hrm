ef96de9005e3137b91cc691b238311eb0a0b735a
Enock <enock.mokua@student.moringaschool.com>
2023-04-01 03:44:47 +0300

    clraete function to navigate user to departent

--- src/Pages/client/signup/signup.js
+++ src/Pages/client/signup/signup.js
@@ -1,7 +1,15 @@
 import "./signup.css";
 import ClientBar from "../navbar/navbar";
 import ClientHeader from "../home/header";
+import { useNavigate } from "react-router-dom";
+
 export default function ClientSignup() {
+  let navigate = useNavigate();
+
+  function redrectDepartment() {
+    navigate("/client/signup/:id");
+  }
+  
   return (
     <section id="client-cont">
       <ClientBar />
@@ -10,6 +18,12 @@ export default function ClientSignup() {
         <section id="client-payment-cont">
           <div id="client-payment-cont1">
             <h3>Release Employees Salaries</h3>
+            <div className="payment-list">
+              <span className="payment-list-head">1</span>
+              <span className="payment-list-body">
+                <p>Engineering Department</p>
+              </span>
+            </div>
           </div>
           <aside id="client-payment-cont2"></aside>
         </section>
